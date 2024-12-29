34966db73bae94297af1d136bd1e178025112d16
thomas.kuhn <thomas.kuhn@zoho.eu>
2024-12-29 18:02:04 +0100

    Add shortening

--- internal/controllers/shorten.go
+++ internal/controllers/shorten.go
@@ -1,10 +1,22 @@
 package controllers
 
 import (
+	"crypto/sha256"
+	"encoding/base64"
+	"fmt"
 	"html/template"
 	"net/http"
 	"strings"
 )
+
+type ShortenedUrlData struct {
+	ShortURL    string
+	OriginalURL string
+}
+
+type Service struct {
+	mapping map[string]string
+}
 
 func ShowShorten(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
@@ -22,10 +34,12 @@ func ShowShorten(w http.ResponseWriter, r *http.Request) {
 		originalURL = "https://" + originalURL
 	}
 
-	// implement shorten url
+	svc := NewService()
+	_ = svc.Shorten(originalURL)
 
-	data := map[string]string{
-		"ShortURL": originalURL,
+	data := ShortenedUrlData{
+		ShortURL:    svc.mapping[originalURL],
+		OriginalURL: originalURL,
 	}
 
 	tmpl, err := template.ParseFiles("internal/template/shorten.html")
@@ -39,4 +53,29 @@ func ShowShorten(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+}
+
+func NewService() *Service {
+	return &Service{
+		mapping: make(map[string]string),
+	}
+}
+
+func (s *Service) Shorten(originalURL string) error {
+	originalURL = strings.TrimSpace(originalURL)
+	hashed := hashURL(originalURL)
+	shortenedURL := fmt.Sprintf(
+		"%s/%s",
+		"cust.om",
+		hashed,
+	)
+
+	s.mapping[originalURL] = shortenedURL
+	return nil
+}
+
+func hashURL(url string) string {
+	hasher := sha256.New()
+	hasher.Write([]byte(url))
+	return base64.URLEncoding.EncodeToString(hasher.Sum(nil))[:8] // Use first 8 characters
 }
